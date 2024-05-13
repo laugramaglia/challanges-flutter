0a30fb7fd50209a0e59a39d3a1c0c2afc1dcffb0
Laureano <laugramaglia@gmail.com>
2024-05-13 09:44:11 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,29 @@
+# Interview Challenge
+
+This is a simple Flutter challenges.
+
+## Scenario
+You are tasked with creating a simple application that fetches data from a local JSON file as if it were a normal endpoint. The application should handle errors during data loading and manage the data state accordingly.
+
+## Requirements
+- Create a simple the cards ui andd fill it with the feached data from a local JSON file.
+- Implement error handling mechanisms to deal with issues during the data fetching process.
+- Manage the data state to provide feedback to the user about the loading status and display the fetched data once it's available.
+
+## Evaluation Criteria
+Your solution will be evaluated based on the following criteria:
+- Correctness: Does the application fetch data from the local JSON file and handle errors appropriately?
+- Error Handling: How effectively does the application handle errors during data loading?
+- Data State Management: Is the data state managed properly to provide feedback to the user?
+- Code Quality: Is the code well-structured, readable, and documented?
+
+## Submission Guidelines
+- Submit your solution as a GitHub repository or a zip file containing the source code.
+
+## Deadline
+Please submit your solution by one hour. Late submissions may not be considered.
+
+## Extras
+
+- json data: lib/data_json.dart
+- figma card: [link](https://www.figma.com/file/vaRSZXvJEutAziBjX64Xi6/Untitled?type=design&node-id=1%3A19&mode=design&t=LPHBlvdWvfKW62wd-1)
---
+++ lib/data_json.dart
@@ -0,0 +1,124 @@
+const jsonData = {
+  "ok": true,
+  "value": [
+    {
+      "id": 40,
+      "name": "Brain Boosters Academy",
+      "description": "Strategies for enhancing cognitive abilities.",
+      "imageUrl":
+          "https://vera-assets.s3.eu-central-1.amazonaws.com/communities/Brain+Boosters+Academy.webp",
+      "slug": "brain-boosters-academy",
+      "published": true
+    },
+    {
+      "id": 41,
+      "name": "Mindful Masters",
+      "description": "Fostering mindfulness and present-moment awareness.",
+      "imageUrl":
+          "https://vera-assets.s3.eu-central-1.amazonaws.com/communities/Mindfulness.webp",
+      "slug": "mindful-masters",
+      "published": true
+    },
+    {
+      "id": 42,
+      "name": "Focus & Flow",
+      "description": "Techniques and tips for improving focus.",
+      "imageUrl":
+          "https://vera-assets.s3.eu-central-1.amazonaws.com/communities/Focus+%26+Flow.webp",
+      "slug": "focus-and-flow",
+      "published": true
+    },
+    {
+      "id": 43,
+      "name": "Entrepreneur Elevators",
+      "description": "Motivation and insights for budding entrepreneurs.",
+      "imageUrl":
+          "https://vera-assets.s3.eu-central-1.amazonaws.com/communities/Entrepreneurial+success.webp",
+      "slug": "entrepreneur-elevators",
+      "published": true
+    },
+    {
+      "id": 44,
+      "name": "Spiritual Satsanga",
+      "description": "A community for spiritual growth and exploration.",
+      "imageUrl":
+          "https://vera-assets.s3.eu-central-1.amazonaws.com/communities/Spiritual+Satsanga.webp",
+      "slug": "spiritual-satsanga",
+      "published": true
+    },
+    {
+      "id": 45,
+      "name": "Financial Freedom Fighters",
+      "description": "Tips and support for achieving financial independence.",
+      "imageUrl":
+          "https://vera-assets.s3.eu-central-1.amazonaws.com/communities/Reaching+financial+freedom.webp",
+      "slug": "financial-freedom-fighters",
+      "published": true
+    },
+    {
+      "id": 46,
+      "name": "ADHD Allies",
+      "description": "Support and understanding for those experiencing ADHD.",
+      "imageUrl":
+          "https://vera-assets.s3.eu-central-1.amazonaws.com/communities/ADHD+Allies.webp",
+      "slug": "adhd-allies",
+      "published": true
+    },
+    {
+      "id": 47,
+      "name": "Career Catalysts",
+      "description": "Guidance and support for career advancement.",
+      "imageUrl":
+          "https://vera-assets.s3.eu-central-1.amazonaws.com/communities/Career+coaching.webp",
+      "slug": "career-catalysts",
+      "published": true
+    },
+    {
+      "id": 48,
+      "name": "Burnout Busters",
+      "description":
+          "Support for individuals dealing with work-related burnout.",
+      "imageUrl":
+          "https://vera-assets.s3.eu-central-1.amazonaws.com/communities/Burnout+and+stress.webp",
+      "slug": "burnout-busters",
+      "published": true
+    },
+    {
+      "id": 49,
+      "name": "Emotional Aid Volunteering",
+      "description": "Aid and support for managing emotional crises.",
+      "imageUrl":
+          "https://vera-assets.s3.eu-central-1.amazonaws.com/communities/2.+Emotional+First+Aid+Providers.webp",
+      "slug": "emotional-aid-volunteering",
+      "published": true
+    },
+    {
+      "id": 50,
+      "name": "Couples' Connection Hub",
+      "description": "A platform for couples seeking deeper connections.",
+      "imageUrl":
+          "https://vera-assets.s3.eu-central-1.amazonaws.com/communities/28.+Couples+Connection+Hub.webp",
+      "slug": "couples-connection-hub",
+      "published": true
+    },
+    {
+      "id": 51,
+      "name": "Feeling Good Circle",
+      "description": "A positive space for cultivating joy and well-being.",
+      "imageUrl":
+          "https://vera-assets.s3.eu-central-1.amazonaws.com/communities/Feeling+Good+Circle.webp",
+      "slug": "feeling-good-circle",
+      "published": true
+    },
+    {
+      "id": 52,
+      "name": "Self-Esteem Builders",
+      "description":
+          "Encouragement for individuals looking to boost their self-esteem.",
+      "imageUrl":
+          "https://vera-assets.s3.eu-central-1.amazonaws.com/communities/Building+self-esteem.webp",
+      "slug": "self-esteem-builders",
+      "published": true
+    }
+  ]
+};
